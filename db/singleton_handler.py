ae65e6dea4fa2e000c7b17d9d3d56f72035eed3f
Fazekas László <fazekas@inf.u-szeged.hu>
2023-02-17 18:13:49 +0100

    remoced lru_cache due to host error

--- db/singleton_handler.py
+++ db/singleton_handler.py
@@ -1,11 +1,9 @@
-from functools import lru_cache
 from typing import Optional
 
 from db.main import BaseDbHandler
 
 __db_handler: Optional[BaseDbHandler] = None # NOTE: real singletion in future?
 
-@lru_cache
 def global_db_handler(setup: Optional[BaseDbHandler] = None) -> BaseDbHandler:
     global __db_handler
 

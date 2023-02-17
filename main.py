a9e3d91b030f2cc3dd87a5825537e304f0ea9580
Fazekas László <fazekas@inf.u-szeged.hu>
2023-02-17 18:08:10 +0100

    added cors

--- main.py
+++ main.py
@@ -2,12 +2,24 @@ from typing import Iterable
 
 from dotenv import load_dotenv
 from fastapi import APIRouter, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from db.main import DbHandler
 from db.singleton_handler import global_db_handler
 from routes import ROUTERS
+from scripts.shared.dotenv_data import (AllowedEnvKey, get_cors_conf,
+                                        get_env_data)
 
 app = FastAPI()
+load_dotenv()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = get_cors_conf(AllowedEnvKey.ALLOW_ORIGINS),
+    allow_credentials = str(get_env_data(AllowedEnvKey.ALLOW_CREDENTIALS)).lower() == "true",
+    allow_methods = get_cors_conf(AllowedEnvKey.ALLOW_METHODS),
+    allow_headers = get_cors_conf(AllowedEnvKey.ALLOW_HEADERS)
+)
 
 def include_routers(routers: Iterable[APIRouter] = ROUTERS) -> None:
     "Includes the given routers into the global app"
@@ -17,6 +29,5 @@ def include_routers(routers: Iterable[APIRouter] = ROUTERS) -> None:
 @app.on_event("startup")
 def startup() -> None:
     "Initializes the server startup requirements"
-    load_dotenv()
     global_db_handler(DbHandler())
     include_routers()

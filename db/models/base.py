ee70f7a17525ab3d9a1233cc9ce7547025cf5558
Fazekas László <fazekas@inf.u-szeged.hu>
2023-02-17 12:18:48 +0100

    added converterts

--- db/models/base.py
+++ db/models/base.py
@@ -1,3 +1,6 @@
+from typing import TYPE_CHECKING, Any, Dict, List, Optional, Set
+
+from pydantic import BaseModel
 from sqlalchemy.ext.declarative import declarative_base
 from typing_extensions import Protocol  # python3.7
 
@@ -6,3 +9,23 @@ Base = declarative_base()
 
 class IDBModel(Protocol):
     __tablename__: str
+
+    if TYPE_CHECKING:
+        def __init__(self, *args: List[Any], **kwargs: Dict[str, Any]) -> None:
+            ...
+
+    @classmethod
+    def convertFromPydanticObject(
+        cls,
+        pydantic_object: BaseModel,
+        exclude: Optional[Set[str]] = None,
+        include: Optional[Dict[str, Any]] = None
+    ):
+        config_dict = pydantic_object.dict(
+            exclude = set() if exclude is None else exclude
+        )
+
+        if include is not None:
+            config_dict.update(include)
+
+        return cls(**config_dict)

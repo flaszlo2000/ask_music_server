e13661bb06bd891f54311dd299d5113e8f800b02
Fazekas László <fazekas@inf.u-szeged.hu>
2023-03-03 23:16:44 +0100

    working jwt

--- readme.md
+++ readme.md
@@ -6,7 +6,6 @@
         - [] send notification 
 - send record request from admin to a event that is not ongoing
 - record blacklist
-- jwt login
 - delete record
 
 
--- routes/v1/main.py
+++ routes/v1/main.py
@@ -1,10 +1,12 @@
+from http import HTTPStatus
 from typing import Optional
 from uuid import UUID
 
-from fastapi import APIRouter, Body, Path, Security
+from fastapi import APIRouter, Body, HTTPException, Path, Security
 
 from pydantic_models.event import EventModelWithId
 from scripts.dependencies import checked_token
+from scripts.shared.security import Token, create_access_token
 from scripts.v1.add_new_record import new_record
 from scripts.v1.get_event_details import (is_correct_password_for_event,
                                           ongoing_event)
@@ -15,9 +17,27 @@ router = APIRouter()
 def get_ongoin_event():
     return ongoing_event()
 
-@router.post("/event_login/{event_id}", response_model = bool)
-def try_to_login_to_event(event_id: UUID = Path(...), event_password: str = Body()):
-    return is_correct_password_for_event(event_id, event_password)
+@router.post("/event_login/{event_id}", response_model = Token)
+def try_to_login_to_event(
+    event_id: UUID = Path(...),
+    user_random_id: str = Body(min_length = 6),
+    event_password: str = Body()
+):
+    if not is_correct_password_for_event(event_id, event_password):
+        raise HTTPException(
+            status_code = HTTPStatus.UNAUTHORIZED,
+            detail = "Incorrect password",
+            headers = {"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token = create_access_token(
+        data = {
+            "sub": user_random_id,
+            "scopes": ["user"]
+        }
+    )
+
+    return Token(access_token = access_token)
 
 @router.post("/record_request/{event_id}")
 def send_record_request(

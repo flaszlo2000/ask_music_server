f1d3589c65155d73b467ec62115af4bb7118805e
Fazekas László <fazekas@inf.u-szeged.hu>
2023-04-07 00:04:04 +0200

    improved security by reducing timing attack possibility

--- scripts/shared/twofactor/auth_code.py
+++ scripts/shared/twofactor/auth_code.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 from datetime import datetime, timedelta
 from random import choice
+from secrets import compare_digest
 from typing import Final
 from uuid import uuid4
 
@@ -23,12 +24,15 @@ class AuthCode:
     def __eq__(self, __o: object) -> bool:
         if not isinstance(__o, AuthCode) and not isinstance(__o, str): return False
 
-        return self.code == str(__o)
+        input_pwd: str = str(__o)
+        if not input_pwd.isascii():
+            # compare_digest only accepts ascii chars
+            return False
+
+        return compare_digest(self.code, input_pwd)
 
     def __ne__(self, __o: object) -> bool:
-        if not isinstance(__o, AuthCode) and not isinstance(__o, str): return True
-        
-        return self.code != str(__o)
+        return not self.__eq__(__o)
 
     def __hash__(self) -> int:
         return id(self) # hm

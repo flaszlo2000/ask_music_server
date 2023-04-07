d20c250fa726c65060d00270a320860073b2292f
Fazekas László <fazekas@inf.u-szeged.hu>
2023-04-07 19:01:52 +0200

    fixed unique checking at add admin

--- scripts/v1/admin_handling.py
+++ scripts/v1/admin_handling.py
@@ -1,63 +1,30 @@
 from http import HTTPStatus
-from typing import List
+from typing import Iterable, List
 
 from fastapi import HTTPException
 from sqlalchemy.orm import Query, Session
 
 from db.models.admins import DBAdmins
 from db.singleton_handler import global_db_handler
-from pydantic_models.admin import (AdminModel, DetailedAdminModel,
-                                   FullAdminModel)
+from pydantic_models.admin import (AdminConfigModel, AdminModel,
+                                   DetailedAdminModel, FullAdminModel)
 from scripts.shared.check_id_in_db import check_id_in_db
 from scripts.shared.security import hash_pwd
 
 
-def get_all_admins() -> List[AdminModel]:
-    "Gets each admin from db and converts it into safer model to respond with"
-    result: List[AdminModel] = list()
-    db_handler = global_db_handler()
+#region DBAdmins checking utils
+def __get_unique_value_filters(base_query: Query[DBAdmins], admin_config_model: AdminConfigModel) -> Iterable[Query[DBAdmins]]:
+    unique_filters = (
+        base_query.filter(DBAdmins.username == admin_config_model.username),
+        base_query.filter(DBAdmins.webhooks_url == admin_config_model.webhooks_url)
+    )
 
-    with db_handler.session() as session:
-        for db_admin in session.query(DBAdmins).all():
-            result.append(
-                AdminModel.convertFromOrm(db_admin)
-            )
+    return unique_filters
 
-    return result
-
-def add_admin_to_db(admin_credentials: DetailedAdminModel) -> None:
-    "Adds an admin to the db with the original password additionally hashed"
-    db_handler = global_db_handler()
-
-    with db_handler.session() as session:
-        record_with_the_same_name = session \
-            .query(DBAdmins) \
-            .filter(DBAdmins.username == admin_credentials.username) \
-            .first()
-        
-        #! FIXME: check webhooks!!!!
-
-        if record_with_the_same_name is not None:
-            raise HTTPException(HTTPStatus.BAD_REQUEST, "Username taken")
-
-        session.add(
-            DBAdmins.convertFromPydanticObject(
-                admin_credentials,
-                exclude = set("password"),
-                include = {"password": hash_pwd(admin_credentials.password)}
-            )
-        )
-
-        session.commit()
-
-#region change_admin_in_db checking utils
 def __check_admin_unique_values(session: Session, updated_admin_model: FullAdminModel) -> None:
     "Check if a change would violate any unique fields in the admins table"
     base_query = session.query(DBAdmins)
-    unique_filters = (
-        base_query.filter(DBAdmins.username == updated_admin_model.username),
-        base_query.filter(DBAdmins.webhooks_url == updated_admin_model.webhooks_url)
-    )
+    unique_filters = __get_unique_value_filters(base_query, updated_admin_model)
 
     for unique_filter in unique_filters:
         violating_instance = unique_filter.filter(DBAdmins.id != updated_admin_model.id).first()
@@ -101,6 +68,38 @@ def get_admin_from_db_checked(
 
     return admin_in_db
 #endregion
+
+def get_all_admins() -> List[AdminModel]:
+    "Gets each admin from db and converts it into safer model to respond with"
+    result: List[AdminModel] = list()
+    db_handler = global_db_handler()
+
+    with db_handler.session() as session:
+        for db_admin in session.query(DBAdmins).all():
+            result.append(
+                AdminModel.convertFromOrm(db_admin)
+            )
+
+    return result
+
+def add_admin_to_db(admin_credentials: DetailedAdminModel) -> None:
+    "Adds an admin to the db with the original password additionally hashed"
+    db_handler = global_db_handler()
+
+    with db_handler.session() as session:
+        for unique_filter in __get_unique_value_filters(session.query(DBAdmins), admin_credentials):
+            if unique_filter.first() is not None:
+                raise HTTPException(HTTPStatus.BAD_REQUEST, "Unique value violation")
+
+        session.add(
+            DBAdmins.convertFromPydanticObject(
+                admin_credentials,
+                exclude = set("password"),
+                include = {"password": hash_pwd(admin_credentials.password)}
+            )
+        )
+
+        session.commit()
 
 def change_admin_in_db(updated_admin_model: FullAdminModel) -> FullAdminModel:
     "Updates admin in db"
